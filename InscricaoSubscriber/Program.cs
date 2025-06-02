89af67d3f8a03aee24c923152ff82e9dc7195b42
IsaEstellaa <61203177+IsaEstellaa@users.noreply.github.com>
2025-06-01 23:24:24 -0300

    adicionando comentarios

--- InscricaoSubscriber/Program.cs
+++ InscricaoSubscriber/Program.cs
@@ -10,13 +10,17 @@ namespace SubscriberInscricao
     {
         public static void Main(string[] args)
         {
+            // configuração do db context
             var optionsBuilder = new DbContextOptionsBuilder<SqlContext>();
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SistemaRH;Trusted_Connection=True;");
 
             using var context = new SqlContext(optionsBuilder.Options);
+            // repositorio de inscricao
             IInscricaoRepository inscricaoRepository = new InscricaoRepository(context);
 
+            // subscriber de inscricao
             var subscriber = new InscricaoSubscriber(inscricaoRepository);
+            // chamada do método
             subscriber.EscutarFila();
         }
     }

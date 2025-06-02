5fa7d013ab49e21ab566ca4ccd497b54f206b606
IsaEstellaa <61203177+IsaEstellaa@users.noreply.github.com>
2025-06-01 23:34:41 -0300

    adicionando comentarios

--- ProcessoSubscriber/Program.cs
+++ ProcessoSubscriber/Program.cs
@@ -10,13 +10,17 @@ namespace SubscriberProcesso
     {
         public static void Main(string[] args)
         {
+            // configuração do db context
             var optionsBuilder = new DbContextOptionsBuilder<SqlContext>();
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SistemaRH;Trusted_Connection=True;");
 
             using var context = new SqlContext(optionsBuilder.Options);
+            // repositorio de processos seletivos
             IProcessoRepository processoRepository = new ProcessoRepository(context);
 
+            // subscriber de processos
             var subscriber = new ProcessoSubscriber(processoRepository);
+            // chamada do método
             subscriber.EscutarFila();
         }
     }

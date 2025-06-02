d2a24009cdba965ebfcef5b960b3c025963d040d
IsaEstellaa <61203177+IsaEstellaa@users.noreply.github.com>
2025-06-01 23:36:36 -0300

    adicionando comentarios

--- VagaSubscriber/Program.cs
+++ VagaSubscriber/Program.cs
@@ -10,13 +10,17 @@ namespace SubscriberVaga
     {
         public static void Main(string[] args)
         {
+            // configuração do db context
             var optionsBuilder = new DbContextOptionsBuilder<SqlContext>();
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SistemaRH;Trusted_Connection=True;");
 
             using var context = new SqlContext(optionsBuilder.Options);
+            // repositorio de vaga
             IVagaRepository vagaRepository = new VagaRepository(context);
 
+            // subscriber de vaga
             var subscriber = new VagaSubscriber(vagaRepository);
+            // chamada do método
             subscriber.EscutarFila();
         }
     }
